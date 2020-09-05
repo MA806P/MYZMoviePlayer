535642c865835a73cb91f11fc52dba8ee75e8acd
MA806P <mayp126@126.com>
2020-09-05 10:00:57 +0800

    project init

---
+++ MYZPlayer/MYZPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MYZPlayer
+//
+//  Created by MA806P on 2020/9/5.
+//  Copyright © 2020 myz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
