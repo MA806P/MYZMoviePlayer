d7fef8ca41b58316a4cc8310f9f24d3fb3bda7ab
MA806P <mayp126@126.com>
2020-09-22 23:35:23 +0800

    add tabbar image

---
+++ MYZPlayer/MYZPlayer/MYZConst.swift
@@ -0,0 +1,12 @@
+//
+//  MYZConst.swift
+//  MYZPlayer
+//
+//  Created by MA806P on 2020/9/22.
+//  Copyright © 2020 myz. All rights reserved.
+//
+
+import Foundation
+
+
+
--- MYZPlayer/MYZPlayer/MYZTabarController.swift
+++ MYZPlayer/MYZPlayer/MYZTabarController.swift
@@ -12,15 +12,15 @@ class MYZTabarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.tabBar.isOpaque = false
         
         let vc1 = UIViewController.init()
-        vc1.view.backgroundColor = UIColor.darkGray
-        self.addChildRootController(vc1, "网络", "", "")
+        vc1.view.backgroundColor = UIColor.white
+        self.addChildRootController(vc1, "网络", "tabbar_network_normal", "tabbar_network_selected")
         
         let vc2 = UIViewController.init()
         vc2.view.backgroundColor = UIColor.darkGray
-        self.addChildRootController(vc2, "文件", "", "")
+        self.addChildRootController(vc2, "文件", "tabbar_file_normal", "tabbar_file_selected")
         
     }
     
@@ -28,8 +28,10 @@ class MYZTabarController: UITabBarController {
     func addChildRootController(_ vc: UIViewController, _ title: String, _ icon: String, _ selectedIcon: String) {
         
         vc.title = title
-        vc.tabBarItem.image = UIImage.init(named: icon)
-        vc.tabBarItem.selectedImage = UIImage.init(named: selectedIcon)
+        vc.tabBarItem.image = UIImage.init(named: icon)!.withRenderingMode(.alwaysOriginal)
+        vc.tabBarItem.selectedImage = UIImage.init(named: selectedIcon)!.withRenderingMode(.alwaysOriginal)
+        vc.tabBarItem.setTitleTextAttributes([.foregroundColor : UIColor(named: "theme_normal")!], for: .normal)
+        vc.tabBarItem.setTitleTextAttributes([.foregroundColor : UIColor(named: "theme_selected")!], for: .selected)
         let nav = MYZNavigationController.init(rootViewController: vc)
         self.addChild(nav)
     }
